11f579ae9715cc4e038fd8b084bfd81c8f726c8c
Ryzen5-3600 <jordanluders@gmail.com>
2020-11-04 14:34:02 -0600

    Change for n8013.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,13 +20,13 @@
 # Inline kernel building
 TARGET_KERNEL_CONFIG := lineageos_n8013_defconfig
 
-N8010 := true
+N8013 := true
 
 WITHOUT_RIL := true
 
 # Selinux
 BOARD_SEPOLICY_DIRS += \
-    device/samsung/n8000/selinux
+    device/samsung/n8013/selinux
 
 # assert
-TARGET_OTA_ASSERT_DEVICE := c0,p4notewifi,p4notewifiww,n8013,GT-N8013,n8010,GT-N8010
+TARGET_OTA_ASSERT_DEVICE := c0,p4notewifi,p4notewifiww,n8013,GT-N8013
