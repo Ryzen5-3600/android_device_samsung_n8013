a8cc66e4a86626af95b3b67effbb2cca9a812695
Ryzen5-3600 <jordanluders@gmail.com>
2020-11-05 13:08:28 -0600

    Fix hardware/samsung.

--- lineage_n8013.mk
+++ lineage_n8013.mk
@@ -2,18 +2,19 @@
 PRODUCT_RELEASE_NAME := n8013
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
 $(call inherit-product, device/samsung/n8013/n8013.mk)
+$(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
+$(call inherit-product, device/samsung/smdk4412-common/common.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1024
 TARGET_SCREEN_WIDTH := 600
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n8013
