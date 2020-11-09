784ec46d4d173ff0c937f22b7c4d3a412b225479
Ryzen5-3600 <jordanluders@gmail.com>
2020-11-08 20:53:46 -0600

    Inherit wifi only products instead of phone products.

--- lineage_n8013.mk
+++ lineage_n8013.mk
@@ -6,15 +6,15 @@ $(call inherit-product, device/samsung/n8013/n8013.mk)
 $(call inherit-product, device/samsung/n80xx-common/n80xx-common.mk)
 $(call inherit-product, device/samsung/smdk4412-common/common.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1024
 TARGET_SCREEN_WIDTH := 600
 
-# Inherit some common stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := n8013
